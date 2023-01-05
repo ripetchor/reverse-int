c99edc48ee842d684dfdf529f687c9694dd0510f
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2023-01-05 22:26:30 +0500

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return Number(
+        String(n)
+            .split("")
+            .reverse()
+            .filter((num) => num !== "-")
+            .join("")
+    );
+};
